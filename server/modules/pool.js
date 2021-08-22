b590d46b968d26b17950930917ef6b8cde6d7967
dane <dane.m.petersen@gmail.com>
2021-08-22 16:00:42 -0500

    deployed to heroku

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -28,11 +28,12 @@ if (process.env.DATABASE_URL) {
 } else {
     // only change the things on the right side of the ||
     config = {
-        user: process.env.PG_USER || null, //env var: PGUSER
-        password: process.env.DATABASE_SECRET || null, //env var: PGPASSWORD
-        host: process.env.DATABASE_SERVER || 'localhost', // Server hosting the postgres database
+        user: process.env.PG_USER || 'jqoiunnyeyjycr', //env var: PGUSER
+        password: process.env.DATABASE_SECRET || '1cd3e9514f797124521a4d2714456bdaf5a893b504de6eedf9c464c125915f20', //env var: PGPASSWORD
+        host: process.env.DATABASE_SERVER || 'ec2-34-230-115-172.compute-1.amazonaws.com', // Server hosting the postgres database
         port: process.env.DATABASE_PORT || 5432, //env var: PGPORT
-        database: process.env.DATABASE_NAME || 'prime_feedback', //env var: PGDATABASE or the name of your database (e.g. database: process.env.DATABASE_NAME || 'koala_holla',)
+        database: process.env.DATABASE_NAME || 'de55a38bbv385r', //env var: PGDATABASE or the name of your database (e.g. database: process.env.DATABASE_NAME || 'koala_holla',)
+        ssl: { rejectUnauthorized: false },
         max: 10, // max number of clients in the pool
         idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
     };
