5c2139d440168980ed3fc7e855cb4450c823d7d0
dane <dane.m.petersen@gmail.com>
2021-08-22 11:39:47 -0500

    fixed issue with building out reducer object, correct way to spread object into another object

--- src/index.js
+++ src/index.js
@@ -6,52 +6,24 @@ import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
 import { combineReducers, createStore } from 'redux';
 
-// HOW FEELING REDUCER
-// const howFeeling = (state = 0, action) => {
-//     if(action.type ==="HOW_FEELING") {
-//         return action.payload;
-//     }
-//     return state;
-// }
-
-// // UNDERSTANDING REDUCER
-// const understanding = (state = 0, action) => {
-//     if(action.type ==="UNDERSTANDING") {
-//         return action.payload;
-//     }
-//     return state;
-// }
-
-// // SUPPORTED REDUCER
-// const supported = (state = 0, action) => {
-//     if(action.type ==="SUPPORTED") {
-//         return action.payload;
-//     }
-//     return state;
-// }
-
-// // COMMENTS REDUCER
-// const comments = (state = '', action) => {
-//     if(action.type ==="COMMENTS") {
-//         return action.payload;
-//     }
-//     return state;
-// }
-
+/**
+ * global Reducer
+ * Builds an object in state that holds our multi-page input values
+ */
 const global = (state = {}, action) => {
-    return {...state, feedback: action.payload}
+    console.log('action',action.payload);
+    return {...state, ...action.payload}
 }
 
+/**
+ * Store
+ * 
+ */
 const store = createStore(
   combineReducers({
-    // howFeeling,
-    // understanding,
-    // supported,
-    // comments,
     global
   })
 );
-
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
