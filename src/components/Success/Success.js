cbc983c52ada962c50d06f5a5740c82621ecb608
dane <dane.m.petersen@gmail.com>
2021-08-22 21:11:52 -0500

    thank you

--- src/components/Success/Success.js
+++ src/components/Success/Success.js
@@ -3,7 +3,7 @@ function Success() {
   const history = useHistory();
   return (
     <>
-      <h1>Thank You!</h1>
+      <h1 className='thank-you'>Thank You!</h1>
       <button className="btn btn-primary" onClick={() => history.push('/')}>Leave New FeedBack</button>
     </>
   )
